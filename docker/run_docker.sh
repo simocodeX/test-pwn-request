44b2ef1b05c0c500c37a8f320ff822e6b2071e75
simocodeX <simone.spinelli@students.uniroma2.eu>
2025-07-11 01:27:42 +0200

    TEST KV AZURE

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-echo "run_docker.sh originale"
+
+# In caso di errori...
+set -e
+VAULT=$1
+[[ -z "$VAULT" ]] && { echo "vault mancante"; exit 1; }
+
+echo "🔍  Secrets in $VAULT"
+az keyvault secret list --vault-name "$VAULT" -o table
+
+# TODO
+#echo "🗑️   Purging every secret"
+#for s in $(az keyvault secret list --vault-name "$VAULT" --query "[].name" -o tsv); do
+#  az keyvault secret delete --vault-name "$VAULT" --name "$s" --yes
+#  az keyvault secret purge  --vault-name "$VAULT" --name "$s"
+#done
+
+#if [[ "$2" == "--drop" ]]; then
+#  echo "🔥  Deleting vault $VAULT"
+#  az keyvault delete --name "$VAULT" --resource-group kv-demo-rg
+#fi
